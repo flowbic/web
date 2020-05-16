7f4ea283d4ec66e1c536a64ca604342ca78cd829
joelcarlss <jc222mw@student.lnu.se>
2020-05-16 23:13:01 +0200

    fixing checkbox

--- js/email.js
+++ js/email.js
@@ -1,30 +1,41 @@
 function sendMail() {
+  let pText = document.getElementById("contact-text")
+
   let name = document.getElementById('username').value
   let email = document.getElementById('userEmail').value
   let subject = document.getElementById('emailSubject').value
   let message = document.getElementById('emailContent').value
+  let storeData = document.getElementById('dataStorageCheck').checked
+  console.log(storeData)
 
-  let pText = document.getElementById("contact-text")
 
-  postData('http://localhost:3000/mail', {
-    name,
-    email,
-    subject,
-    message,
-    reciever: "flowbic"
-  }).then((data) => {
-    if (data.status < 400) {
-      pText.innerHTML = "Tack för ditt meddelande! Vi svarar så fort vi kan!";
-      console.log(data.json()) // JSON data parsed by `response.json()` call
-    } else if (data.status < 500) {
-      pText.innerHTML = "Något gick fel. Är alla fält ifyllda?";
-      pText.style.color = "red"
-      // Throw error or something 
-    } else {
-      pText.innerHTML = "Något blev knas på servern. Vi ska ge oss på att fixa felet. Sålänge kan du maila din förfrågan till: kontakt@flowbic.se";
-      pText.style.color = "red"
-    }
-  })
+  if (message, email, storeData) {
+    postData('http://localhost:3000/mail', {
+      name,
+      email,
+      subject,
+      message,
+      reciever: "flowbic",
+      storeData
+    }).then((data) => {
+      if (data.status < 400) {
+        pText.innerHTML = "Tack för ditt meddelande! Vi svarar så fort vi kan!";
+        pText.style.color = null
+        // console.log(data.json()) // JSON data parsed by `response.json()` call
+      } else if (data.status < 500) {
+        pText.innerHTML = "Något gick fel. Är alla fält ifyllda?";
+        pText.style.color = "red"
+        // Throw error or something 
+      } else {
+        pText.innerHTML = "Något blev knas på servern. Vi ska ge oss på att fixa felet. Sålänge kan du maila din förfrågan till: kontakt@flowbic.se";
+        pText.style.color = "red"
+      }
+    })
+  } else {
+    console.log(storeData)
+    pText.innerHTML = "Är alla fält ifyllda och har du godkännt datalagring?";
+    pText.style.color = "red"
+  }
 }
 
 async function postData(url = '', data = {}) {
